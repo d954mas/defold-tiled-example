4a9ad1e1adbffda422bfd73883d0244d39334170
d954mas <d954mas@gmail.com>
2021-08-18 01:32:20 +0300

    add game

---
+++ levels/tilesets/objects.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="objects" tilewidth="128" tileheight="256" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="player"/>
+  </properties>
+  <image width="128" height="256" source="objects/player.png"/>
+ </tile>
+</tileset>
